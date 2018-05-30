a687692cb03413cb60ff8f89d8cf994d7b5dfe33
prabhsharan5911 <39779008+prabhsharan5911@users.noreply.github.com>
2018-05-30 16:34:49 -0400

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author prabhsharan singh
+ */
+public class Helloworld {
+    public static void main(String args[])
+    {System.out.println("helloworld");
+        System.out.println("user"); 
+        System.out.println("firstprogram");
+        int a=10;
+        float f=10.2f;
+        double d=12.32325689;
+        short s=31;
+        char c='a';
+        String str="program";
+        boolean b=true;
+        System.out.println("value of integer a is="+a);
+        System.out.println("value of float f is="+f);
+        System.out.println("value of double d is="+d);
+        System.out.println("value of short s is="+s);
+        System.out.println("value of char c is="+c);
+        System.out.println("value of string str is="+str);
+        
+        a *=20; // a=a*20
+    System.out.println("multiplication of a with 20"+a);
+    a++; // a=a+1;
+    System.out.println("value of a is =" + a);
+    ++a; // a=1002}
+    System.out.println("value of a="+a);
+    --a;
+    System.out.println("value of a = " +a);
+    a--;
+    System.out.println("value of a ="+a);
+    
+   
+}}
